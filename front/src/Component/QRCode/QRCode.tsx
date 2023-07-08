13a6e6e632c4884a1f8f259dca8ad7301a571f4f
crystel1996 <crystelratsimbazafy@gmail.com>
2023-07-08 13:10:40 +0300

    add test unit for text on qr-code component

--- front/src/Component/QRCode/QRCode.tsx
+++ front/src/Component/QRCode/QRCode.tsx
@@ -4,7 +4,15 @@ import { QRCodeComponentInterface } from './interface';
 export const QRCode: FC<QRCodeComponentInterface> = () => {
 
     return <div data-testid="qr-code">
-
+        <div>
+            <img alt="Schema-QR-Code" src="/assets/images/image-qr-code.png" />
+        </div>
+        <div>
+            <h2>Improve your front-end skills by building projects</h2>
+        </div>
+        <div>
+            <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+        </div>
     </div>
 
 }
