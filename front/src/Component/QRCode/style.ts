5a8b3cf9f8bf2b72609e72c619f2fb780b46ab82
crystel1996 <crystelratsimbazafy@gmail.com>
2023-07-08 16:14:38 +0300

    fix center div

--- front/src/Component/QRCode/style.ts
+++ front/src/Component/QRCode/style.ts
@@ -4,10 +4,10 @@ import { COLOR } from '../../Helper';
 const cardStyle = css`
 
     display: flex;
-    flex-direction: column;
     justify-content: center;
     text-align: center;
     align-items: center;
+    height: 100vh;
 `;
 
 const cardContent = css`
@@ -18,8 +18,11 @@ const cardContent = css`
     border-radius: 25px;
     padding: 15px;
     padding-bottom: 25px;
-    margin: 15px 15px 15px 15px;
-
+    @media (max-width: 375px) {
+        
+        width: 280px ;
+        
+    }
 `;
 
 const cardContentImage = css`
@@ -27,7 +30,11 @@ const cardContentImage = css`
         width: 350px;
         border-radius: 25px;
     }
-
+    @media (max-width: 375px) {
+        & img {
+          width: 280px ;
+        }
+    }
 `;
 
 const cardContentTitle = css`
