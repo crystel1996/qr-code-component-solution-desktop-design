0c783b21616ad26013ec969880e08506ab0ff411
crystel1996 <crystelratsimbazafy@gmail.com>
2023-07-08 12:50:02 +0300

    add unit test for qr-code component is not missing

--- front/src/Component/QRCode/QRCode.tsx
+++ front/src/Component/QRCode/QRCode.tsx
@@ -3,6 +3,8 @@ import { QRCodeComponentInterface } from './interface';
 
 export const QRCode: FC<QRCodeComponentInterface> = () => {
 
-    return <></>
+    return <div data-testid="qr-code">
+
+    </div>
 
 }
